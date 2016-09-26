69794db48d7153fdcdf20fcbb0f025d77939c84d
unknown <smadur@d-7fv7tz1.cbi-inet.com>
2016-09-26 15:05:19 -0500

    Initial Commit

---
+++ headlines.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import render_template
+import feedparser
+
+
+app = Flask(__name__)
+
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
+@app.route("/")
+@app.route("/bbc")
+def bbc():
+    return get_news('bbc')
+
+@app.route("/cnn")
+def cnn():
+    return get_news('cnn')
+
+@app.route("/fox")
+def fox():
+    return get_news('fox')
+
+@app.route("/iol")
+def iol():
+    return get_news('iol')
+
+def get_news(publication='bbc'):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return render_template("home.html", articles=feed['entries'])
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
